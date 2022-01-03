21025771841e50657264930c330006b45a26d9c1
JoseCarlos <jose.leon@deister.es>
2022-01-03 08:11:41 +0100

    add lang: ts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,19 @@
 <template>
   <v-container>
-    <input placeholder="nombre" type="text" v-model="name"> <br>
-    <input placeholder="apellido" type="text" v-model="lastname"> <br>
-    <input placeholder="edad" type="number" v-model="edad"> <br>
-    <input placeholder="sexo" type="text" v-model="sexo"> <br>
-    <input type="checkbox" v-model="status"> <br>
 
-
-    <v-text-field label="prueba">
+    <v-text-field label="Nombre" v-model="name">
+        
+    </v-text-field>
+    <v-text-field label="Apellidos" v-model="lastname">
+        
+    </v-text-field>
+    <v-text-field label="Edad" v-model="edad">
+        
+    </v-text-field>
+    <v-text-field label="Sexo" v-model="sexo">
+        
+    </v-text-field>
+    <v-text-field label="Estado" v-model="status">
         
     </v-text-field>
 
@@ -22,95 +28,95 @@
   </v-container>
 </template>
 
-<script>
-import {onMounted, defineComponent, ref} from "@vue/composition-api";
-import dbConnection from "../firebase"
-import { getDocs, collection, addDoc, setDoc, doc, deleteDoc } from 'firebase/firestore';
-const collecion = collection(dbConnection, "usuarios");
+<script lang="ts">
+  import {onMounted, defineComponent, ref} from "@vue/composition-api";
+  import dbConnection from "../firebase"
+  import { getDocs, collection, addDoc, setDoc, doc, deleteDoc } from 'firebase/firestore';
+  const collecion = collection(dbConnection, "usuarios");
 
-export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
+  export default defineComponent({
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
 
-  setup() {
+    setup() {
 
-    /**
-     * Variables
-     */
-    const usuarios = ref([]);
-    const name = ref("");
-    const lastname = ref("");
-    const edad = ref(null);
-    const sexo = ref("M");
-    const status = ref(true);
+      /**
+       * Variables
+       */
+      const usuarios = ref([]);
+      const name = ref("");
+      const lastname = ref("");
+      const edad = ref(null);
+      const sexo = ref("M");
+      const status = ref(true);
 
 
-    /**
-     * Methods
-     */
-    function getUsers() {
-      
-      usuarios.value = [];
-      getDocs(collecion).then((r) => r.docs.forEach(
-         (item) => usuarios.value.push({id:item.id, data:item.data()})
+      /**
+       * Methods
+       */
+      function getUsers() {
+        
+        usuarios.value = [];
+        getDocs(collecion).then((r) => r.docs.forEach(
+          (item) => usuarios.value.push({id:item.id, data:item.data()})
+          )
         )
-      )
 
-      console.log(usuarios.value);
-    }
-    
-    function insertUser(){
-          addDoc(collecion, this.usuario).then(() => {
-              this.getUsers();
+        console.log(usuarios.value);
+      }
+      
+      function insertUser(){
+            addDoc(collecion, this.usuario).then(() => {
+                this.getUsers();
+            });
+        }
+
+      function updateUser(id){
+
+          this.userId = doc(collection(dbConnection, 'usuarios'),id);
+
+          setDoc(this.userId, this.usuario).then(() => {
+                this.getUsers();
           });
+        }
+
+        function deleteUser(id) {
+
+          this.userId = doc(collection(dbConnection, 'usuarios'),id);
+
+          deleteDoc(this.userId).then(() => {
+                this.getUsers();
+          });
+        }
+
+        onMounted(() => {
+          console.log("Cargar method : getUsers");
+          getUsers()
+        })
+
+      /**
+       *  Return
+       */
+
+      return {
+        usuarios,
+        name,
+        lastname,
+        edad,
+        sexo,
+        status,
+
+        // Methods
+        getUsers,
+        insertUser,
+        updateUser,
+        deleteUser
       }
 
-    function updateUser(id){
-
-        this.userId = doc(collection(dbConnection, 'usuarios'),id);
-
-        setDoc(this.userId, this.usuario).then(() => {
-              this.getUsers();
-        });
-      }
-
-      function deleteUser(id) {
-
-        this.userId = doc(collection(dbConnection, 'usuarios'),id);
-
-        deleteDoc(this.userId).then(() => {
-              this.getUsers();
-        });
-      }
-
-      onMounted(() => {
-        console.log("Cargar method : getUsers");
-        getUsers()
-      })
-
-    /**
-     *  Return
-     */
-
-    return {
-      usuarios,
-      name,
-      lastname,
-      edad,
-      sexo,
-      status,
-
-      // Methods
-      getUsers,
-      insertUser,
-      updateUser,
-      deleteUser
     }
-
-  }
-})
+  })
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
